cc57a00be510876509c46f988789aed150044f29
lucaspio1 <lucaspioh@gmail.com>
2022-11-04 19:55:43 -0300

    install Sentry

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const Calculadora = require("./Controller/Rotas")
+const Sentry = require("@sentry/node");
+const SentryTracing = require("@sentry/tracing");
 
 const app = express()
 
 app.listen("3000", ()=>{
     console.log("Iniciamos o Sistema!")
 })
+
+Sentry.init({
+    dsn: "https://f142c53ddedd4cdcb70bb87a5d631b39@o4504103203045376.ingest.sentry.io/4504103302397952",
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+
 
 Calculadora.router(app)
